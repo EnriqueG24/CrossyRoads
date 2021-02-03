7ca475c49c97f56dea07a51e972538236a39217a
Enrique Gongora <EGTech24@gmail.com>
2021-02-02 16:03:38 -0800

    Game fully works

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+screen.onkey(player.up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_cars()
+    car_manager.move()
+
+    if player.ycor() > 290:
+        scoreboard.increase_level()
+        player.reset_player()
+        car_manager.increase_speed()
+
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+screen.exitonclick()
